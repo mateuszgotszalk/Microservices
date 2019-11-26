6bc151183f41fe71ba97c76fa060095e84729efb
Mateusz <mateuszg4@gmail.com>
2019-11-26 14:29:12 +0100

    Working App 1.0

---
+++ credit-service/src/main/java/com/microservices/creditservice/models/CustomerList.java
@@ -0,0 +1,16 @@
+package com.microservices.creditservice.models;
+
+import java.util.List;
+
+public class CustomerList {
+
+    private List<Customer> customers;
+
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(List<Customer> customers) {
+        this.customers = customers;
+    }
+}
