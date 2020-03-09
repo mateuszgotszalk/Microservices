1caa3e84e5efa425154eb1b78f5842fc7477eeb2
Mateusz <mateuszg4@gmail.com>
2020-03-09 23:28:51 +0100

    Working dockers

--- credit-service/src/main/java/com/microservices/creditservice/repositories/CreditRepository.java
+++ credit-service/src/main/java/com/microservices/creditservice/repositories/CreditRepository.java
@@ -2,6 +2,8 @@ package com.microservices.creditservice.repositories;
 
 import com.microservices.creditservice.models.Credit;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CreditRepository extends CrudRepository<Credit, Integer> {
 }
--- product-service/src/main/java/com/microservices/productservice/repositories/ProductRepository.java
+++ product-service/src/main/java/com/microservices/productservice/repositories/ProductRepository.java
@@ -2,7 +2,9 @@ package com.microservices.productservice.repositories;
 
 import com.microservices.productservice.models.Product;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends CrudRepository<Product, String> {
     Product findByCreditId(Integer id);
 }
